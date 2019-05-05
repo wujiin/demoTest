367a1be9b1d03d9f7589e029a7c8eeb192fe4432
wu_jin <979762929@qq.com>
2019-05-06 03:31:33 +0800

    modfiy

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -16,7 +16,7 @@ public class TestController {
     @RequestMapping("test")
     public String test() {
         System.out.println("jenkins修改测试。。。");
-        return "jenkins修改测试";
+        return "jenkins修改测试-111111111111111111";
     }
 
 }
